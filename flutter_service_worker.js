93ca3290f05f6497b54e0a1df1891e742e7fe308
Mohamed Gamal <your_email@example.com>
2024-11-02 12:16:47 +0200

    mod

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -459,9 +459,9 @@ const RESOURCES = {"404.html": "3454ba0e3ba2c4e64d275f6b947d8a7b",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "354269c253583c853248d2940a6bd3d9",
-"/": "354269c253583c853248d2940a6bd3d9",
-"main.dart.js": "d772affda811fce4bbed104fff74fad2",
+"index.html": "e6df9087093d1be6cb045564f1f59c11",
+"/": "e6df9087093d1be6cb045564f1f59c11",
+"main.dart.js": "1e47c8dfc451d62bd6ed1fa5e2813253",
 "manifest.json": "f0422ed3d2d61d549ae4d1aa0f0903e2",
 "version.json": "af296f68e29d82cd5e70fcf685a404bf"};
 // The application shell files that are downloaded before a service worker can
